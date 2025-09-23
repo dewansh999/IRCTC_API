6a50cb48ccc21933f6da510aa92969d0119e11b2
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-09-23 17:27:07 +0530

    Update TicketRestController.java

--- src/main/java/in/expedia/rest/TicketRestController.java
+++ src/main/java/in/expedia/rest/TicketRestController.java
@@ -4,6 +4,14 @@ import java.util.Collection;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import in.expedia.bindings.Passenger;
 import in.expedia.bindings.Ticket;
 import in.expedia.service.TicketService;
